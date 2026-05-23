1b7795511a67c954a6c0f8a24c427492f518649a
Paulo Henrique <phpessoal2021@gmail.com>
2026-05-22 22:08:39 -0300

    feat: criando componente de botão

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    :class="[
+      'group neon-below-lavender relative inline-flex h-12 items-center justify-center rounded-full bg-lavenderPurple-500 px-6 font-semibold text-white transition-all duration-300 ease-out hover:neon-below-mauve',
+      {
+        'cursor-not-allowed opacity-50': disabled,
+      },
+    ]"
+    :disabled="disabled"
+  >
+    <span class="relative z-10 inline-flex items-center justify-center">
+      <slot />
+    </span>
+    <span
+      v-if="!disabled"
+      class="absolute inset-0 overflow-hidden rounded-full"
+      aria-hidden="true"
+    >
+      <span
+        class="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-mauveMagic-600 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"
+      />
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface ButtonProps {
+  disabled?: boolean
+}
+
+withDefaults(defineProps<ButtonProps>(), {
+  disabled: false,
+})
+</script>
